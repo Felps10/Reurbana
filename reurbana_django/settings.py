1a3d1611078488c9c5a5e61e29b441dba7c3e708
FelipeCosta <fac.10@outlook.com>
2020-02-13 12:16:04 -0300

    updated settings.py

--- reurbana_django/settings.py
+++ reurbana_django/settings.py
@@ -120,13 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [os.path.join(PROJECT_ROOT, 'static')],
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# HEROKU SETUP
+# after import os
+#
+# at the bottom of the file
 django_heroku.settings(locals())
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIR = [os.path.join(PROJECT_ROOT, 'static')],
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
